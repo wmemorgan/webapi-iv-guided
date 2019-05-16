add96a4f0af93827c685b8c91c9afb2a31d5fdf3
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-05-16 11:55:48 +0900

    Add MOTD feature

--- api/server.js
+++ api/server.js
@@ -11,7 +11,11 @@ server.use(express.json());
 server.get('/', async (req, res) => {
   try {
     const shoutouts = await db('shoutouts');
-    res.status(200).json(shoutouts);
+    const messageOfTheDay = process.env.MOTD || 'Not today'
+    res.status(200).json({
+      motd: messageOfTheDay,
+      shoutouts
+    });
   } catch (error) {
     console.error('\nERROR', error);
     res.status(500).json({ error: 'Cannot retrieve the shoutouts' });
